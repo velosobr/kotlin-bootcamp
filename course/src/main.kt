decaffc6932cd93c34796aa8db2c930053f7b888
lino.veloso <lino.veloso@dextra-sw.com>
2020-02-24 14:52:54 -0300

    the basic of for-each collection

--- course/src/main.kt
+++ course/src/main.kt
@@ -3,10 +3,21 @@ fun sayHello(greeting: String, itemToGreet: String) = println("$greeting $itemTo
 fun main() {
 
 val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-
+val arrayOfNames= arrayOf("Lino","Karla","Duda","Maria")
     println(interestingThings.size)
     println(interestingThings[0])
     println(interestingThings.get(0))
 
-}
+    for (interestingThing in interestingThings) {
+        println(interestingThing)
+    }
+    //using "it"
+    println("using \"it\"")
+    arrayOfNames.forEach {
+        println(it)
+    }
+//    Using a convencional way
+    println("Using a convencional way")
+    arrayOfNames.forEach { name -> println(name) }
 
+}
