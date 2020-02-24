50e0f7bf90c64c5625a6732b64c08c22c007f190
lino.veloso <lino.veloso@dextra-sw.com>
2020-02-24 16:01:17 -0300

    a bit of List & map collections

--- course/src/main.kt
+++ course/src/main.kt
@@ -1,31 +1,24 @@
+import java.util.*
+
 fun sayHello(greeting: String, itemToGreet: String) = println("$greeting $itemToGreet")
 
 fun main() {
 
 val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
-val arrayOfNames= arrayOf("Lino","Karla","Duda","Maria")
-    println(interestingThings.size)
-    println(interestingThings[0])
-    println(interestingThings.get(0))
+val arrayOfNames= mutableListOf("Lino","Karla","Duda","Bubba","Maria","Bronx")
+    arrayOfNames.add("Neymar")
 
-    for (interestingThing in interestingThings) {
-        println(interestingThing)
+    arrayOfNames.forEach { name ->
+        println(name)
     }
-    //using "it"
-    println("using \"it\"")
-    arrayOfNames.forEach {
-        println(it)
-    }
-//    Using a convencional way
-    println("Using a convencional way")
-    arrayOfNames.forEach { name -> println(name) }
-// using index
-    println("prinln(\"\")\n")
 
-    arrayOfNames.forEachIndexed {
-        i, name -> println(
-        "$name is at index $i"
-    )
+    var aliens = TreeMap<String, Int>()
 
-    }
+    aliens["Bubba"] = 123
+
+    aliens["Neiggi"] = 312
+
+    for ((name, age) in aliens)
+        println("The name is $name, and the age is $age")
+
 }
