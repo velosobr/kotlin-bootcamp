c998d8c62cd82dcf88e01b3c0f85e334af1076f8
velosobr <linoc.veloso@gmail.com>
2020-09-06 23:58:29 -0300

    WIP - Studying Sealed classes

---
+++ SealedClasses/src/NotResult.kt
@@ -0,0 +1,1 @@
+object InProgress : Result<Nothing>()
---
+++ SealedClasses/src/Result.kt
@@ -0,0 +1,12 @@
+sealed class Result <out T : Any>{
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object InProgress : Result<Nothing>()
+}
+
+fun handleResult(result: Result<Int>){
+    when(result){
+
+        else ->
+    }
+}
